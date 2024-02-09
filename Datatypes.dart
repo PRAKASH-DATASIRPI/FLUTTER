6e2177217d454d66e4d5e1b05bdf7e2b530feebb
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-02-09 11:38:44 +0530

    done

---
+++ Datatypes.dart
@@ -0,0 +1,10 @@
+void main() {
+  var list = [1, 2, 3];
+  var map = {"key": "value", "key1": "value1"};
+  print(list);
+  print(map);
+    var heart_symbol = '\u2665';  
+    var laugh_symbol = '\u{1f600}';  
+    print(heart_symbol);  
+    print(laugh_symbol);  
+}
