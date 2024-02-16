f17d23fed554af40198ccb667f3223ef1435232e
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-02-16 12:45:17 +0530

    done

---
+++ lib/pages/map_pag.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// class MapPage extends StatefulWidget {
+//   const MapPage({Key? key});
+
+//   @override
+//   State<MapPage> createState() => _MapPageState();
+// }
+
+// class _MapPageState extends State<MapPage> {
+//   static const LatLng _pGoogleplex = LatLng(37.4223, -122.0848);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GoogleMap(
+//         initialCameraPosition: CameraPosition(
+//           target: _pGoogleplex,
+//           zoom: 2,
+//         ),
+//       ),
+//     );
+//   }
+// }
